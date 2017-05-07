c950eb4f620f746327c4dcaab1ea0ae404eb77ba
Mohanix <im.mohanix@gmail.com>
2017-05-07 09:40:26 +0545

    adding home page files

---
+++ include/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Online Voting System</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
