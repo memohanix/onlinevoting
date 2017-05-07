8e3288b4642b5395157d808b1dbd6f777203d62c
Mohanix <im.mohanix@gmail.com>
2017-05-07 10:21:42 +0545

    adding sample form for state creation

--- setting/states.php
+++ setting/states.php
@@ -14,10 +14,30 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="page-header">
-                    <h1>Example page header <small>Subtext for header</small></h1>
+                    <h1>State <small>Creation of new state</small></h1>
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-8">
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Name</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputEmail3" placeholder="Name of state">
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Create</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
     </div>
 
 <?php include '../include/footer.php'; ?>
